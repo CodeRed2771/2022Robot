197e1629823499ef2c807b05f3ef531bc8c4d9a6
CalebB <cmbuff18@gmail.com>
2022-01-27 21:12:41 -0500

    Changed Shooter motor to a CANSparkMax, there are errors and I commited some out

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -15,12 +15,14 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.Timer;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 public class Shooter {
     private static Shooter instance;
     // private static TalonSRX shooterMotor = new TalonSRX(Wiring.SHOOTER>MOTOR_ID);
-    private static TalonFX shooterMotor = new TalonFX(Wiring.SHOOTER_MOTOR_ID);
+    private static CANSparkMax shooterMotor = new CANSparkMax(Wiring.SHOOTER_MOTOR_ID, MotorType.kBrushless);
     private static Servo gate = new Servo(Wiring.SERVO_PWM_ID);
     // private static Timer timer = new Timer();
     // private static TalonFX shooterMotor1 = new TalonFX(Wiring.SHOOTER1_MOTOR_ID);
@@ -40,33 +42,33 @@ public class Shooter {
     public static void init() {
         // shooterMotor.set(shooterMotor1.follower, Wiring.SHOOTER_MOTOR_ID);
         // shooterMotor1.follow(shooterMotor);
-        shooterMotor.configFactoryDefault(10);
+        //shooterMotor.configFactoryDefault(10);
         shooterMotor.setInverted(true);
-        shooterMotor.setSensorPhase(false);
+        //shooterMotor.setSensorPhase(false);
        
 		/* set the relevant frame periods to be at least as fast as periodic rate */
 		// shooterMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 0);
         // shooterMotor.setStatusFramePeriod(StatusFrameEnhanced., 10, 0);
 
 		/* set the peak and nominal outputs */
-		shooterMotor.configNominalOutputForward(0, 0);
-		shooterMotor.configNominalOutputReverse(0, 0);
-		shooterMotor.configPeakOutputForward(1, 0);
-		shooterMotor.configPeakOutputReverse(-1, 0);
-		shooterMotor.setNeutralMode(NeutralMode.Coast);
-        shooterMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, kPIDLoopIdx, 0);
+		//shooterMotor.configNominalOutputForward(0, 0);
+		//shooterMotor.configNominalOutputReverse(0, 0);
+		//shooterMotor.configPeakOutputForward(1, 0);
+		//shooterMotor.configPeakOutputReverse(-1, 0);
+		//shooterMotor.setNeutralMode(NeutralMode.Coast);
+        //shooterMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, kPIDLoopIdx, 0);
 
-		shooterMotor.configClosedloopRamp(.45, 0);
+		//shooterMotor.configClosedloopRamp(.45, 0);
 		/* set closed loop gains in slot0 - see documentation */
-        shooterMotor.selectProfileSlot(0, 0);
+        //shooterMotor.selectProfileSlot(0, 0);
 
-
-		shooterMotor.config_kP(kPIDLoopIdx, Calibration.SHOOTER_P, 0);
-		shooterMotor.config_kI(kPIDLoopIdx, Calibration.SHOOTER_I, 0);
-		shooterMotor.config_kD(kPIDLoopIdx, Calibration.SHOOTER_D, 0);
-        shooterMotor.config_kF(kPIDLoopIdx, Calibration.SHOOTER_F, 0);
+        
+		//shooterMotor.config_kP(kPIDLoopIdx, Calibration.SHOOTER_P, 0);
+		//shooterMotor.config_kI(kPIDLoopIdx, Calibration.SHOOTER_I, 0);
+		//shooterMotor.config_kD(kPIDLoopIdx, Calibration.SHOOTER_D, 0);
+        //shooterMotor.config_kF(kPIDLoopIdx, Calibration.SHOOTER_F, 0);
 		/* zero the sensor */
-        shooterMotor.setSelectedSensorPosition(0, kPIDLoopIdx, 0);
+        //shooterMotor.setSelectedSensorPosition(0, kPIDLoopIdx, 0);
  	
 		// Current limit
         // shooterMotor.configContinuousCurrentLimit(25);
@@ -94,15 +96,15 @@ public class Shooter {
         // }
 
         if (SmartDashboard.getBoolean("Shooter TUNE", true)) {
-			shooterMotor.config_kF(kPIDLoopIdx, SmartDashboard.getNumber("Shoot F", 0), 0);
-			shooterMotor.config_kP(kPIDLoopIdx, SmartDashboard.getNumber("Shoot P", 0), 0);
-			shooterMotor.config_kI(kPIDLoopIdx, SmartDashboard.getNumber("Shoot I", 0), 0);
-            shooterMotor.config_kD(kPIDLoopIdx, SmartDashboard.getNumber("Shoot D", 0), 0);
+			//shooterMotor.config_kF(kPIDLoopIdx, SmartDashboard.getNumber("Shoot F", 0), 0);
+			//shooterMotor.config_kP(kPIDLoopIdx, SmartDashboard.getNumber("Shoot P", 0), 0);
+			//shooterMotor.config_kI(kPIDLoopIdx, SmartDashboard.getNumber("Shoot I", 0), 0);
+            //shooterMotor.config_kD(kPIDLoopIdx, SmartDashboard.getNumber("Shoot D", 0), 0);
         }
 
             if (isEnabled) {
-                shooterMotor.set(ControlMode.Velocity, SmartDashboard.getNumber("Shoot Setpoint", Calibration.SHOOTER_DEFAULT_SPEED) * adjustmentFactor);
-                SmartDashboard.putNumber("SHOOTER VELOCITY", shooterMotor.getSelectedSensorVelocity());
+                //shooterMotor.set(ControlMode.Velocity, SmartDashboard.getNumber("Shoot Setpoint", Calibration.SHOOTER_DEFAULT_SPEED) * adjustmentFactor);
+                //SmartDashboard.putNumber("SHOOTER VELOCITY", shooterMotor.getSelectedSensorVelocity());
 
                 System.out.println(timer);
 
@@ -129,8 +131,8 @@ public class Shooter {
             }
         
         
-        SmartDashboard.putNumber("Shoot Enc", shooterMotor.getSensorCollection().getIntegratedSensorVelocity());
-        SmartDashboard.putNumber("Shoot Err", shooterMotor.getClosedLoopError());
+        //SmartDashboard.putNumber("Shoot Enc", shooterMotor.getSensorCollection().getIntegratedSensorVelocity());
+        //SmartDashboard.putNumber("Shoot Err", shooterMotor.getClosedLoopError());
         SmartDashboard.putBoolean("Is At Speed", isAtSpeed());
     }
 
@@ -150,7 +152,8 @@ public class Shooter {
         oneShot = false;
         continuousShooting = false;
         resetTimer();
-        shooterMotor.set(ControlMode.PercentOutput,0);
+        //shooterMotor.set(ControlMode.PercentOutput,0);
+        shooterMotor.set(0);
     }
 
     public static void setAdjustmentFactor (double adjustmentFactor) {
@@ -161,7 +164,8 @@ public class Shooter {
 
     public static double getShooterSpeed() {
         // return shooterMotor.getSensorCollection().getIntegratedSensorVelocity();
-        return shooterMotor.getSelectedSensorVelocity();
+        //return shooterMotor.getSelectedSensorVelocity();
+        return 0;
     }
 
     public static boolean isAtSpeed() {
