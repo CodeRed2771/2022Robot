ae36d4f437fe79bbce68492d7cb63c7859c31492
CalebB <cmbuff18@gmail.com>
2022-02-18 17:12:16 -0500

    Worked on Vision Ball  Changed for loop > added i value on smartdashboard to test

--- src/main/java/frc/robot/VisionBall.java
+++ src/main/java/frc/robot/VisionBall.java
@@ -11,9 +11,11 @@ import org.opencv.imgproc.Imgproc;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.vision.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class VisionBall implements VisionRunner.Listener<VisionBallPipelineRed>
 {
+    // Varriables 
     private static UsbCamera  camera;
     private static int IMG_WIDTH = 640;
     private static int IMG_HEIGHT = 480;
@@ -27,13 +29,15 @@ public class VisionBall implements VisionRunner.Listener<VisionBallPipelineRed>
     private static int ballAmount = 0;
     private static double bestScore = 0;
     private static double currentScore = 0;
+    private static double iTest; 
 
     
-
+    // Distance Formula 
     public static double distance(double xPosition, double yPosition) {
         return Math.sqrt(Math.pow(xPosition, 2)+Math.pow(yPosition,2));
     }
     
+    // Vision Processing 
     public static double algorithim(Rect re) {
         double cenX, cenY, dis, width;
         cenX = re.x + (re.width / 2);
@@ -49,23 +53,21 @@ public class VisionBall implements VisionRunner.Listener<VisionBallPipelineRed>
                 closestBall = 0;
                 algorithim(Imgproc.boundingRect(ballsFound.get(0)));
             } else {
-                for (int i = 0; ballsFound.size() > i; i ++) {
-                    algorithim(Imgproc.boundingRect(ballsFound.get(i))); // Determining Closest Ball; Change as needed
+                for (int i = 0; ballsFound.size() < i; i ++) {
+                    algorithim(Imgproc.boundingRect(ballsFound.get(i))); // Determining Closest Ball; Change as needed/
+                    iTest = i;
+                    SmartDashboard.putNumber("i value", iTest);
                     if (bestScore < currentScore) {
                         bestScore = currentScore;
                         closestBall = i;
                     }
                 }
-
             }
         ballAmount = ballsFound.size();
         return ballsFound.get(closestBall);
     }
 
-    public static int getBallIndex() {
-        return closestBall;
-    }
-
+    // Vision Set-Up and Run
     public static void init(String allianceColor) {
         camera = CameraServer.startAutomaticCapture();
         camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
@@ -102,6 +104,7 @@ public class VisionBall implements VisionRunner.Listener<VisionBallPipelineRed>
                     centerY = IMG_HEIGHT / 2;
                     ballAmount = 0;
                     foundBall.set(false);
+                    bestScore = 0;
                 }
             });
         }
@@ -116,30 +119,7 @@ public class VisionBall implements VisionRunner.Listener<VisionBallPipelineRed>
         visionThread.interrupt();  // not sure this really does any good
     }
 
-    public static int getBallNumber() {
-        return ballAmount;
-    }
-
-    public static double getBallXOffset() {
-        return centerX - (IMG_WIDTH / 2);
-    }
-    public static double getBallYOffset() {
-        return centerY - (IMG_HEIGHT / 2);
-    }
-    @Override
-    public void copyPipelineOutputs(VisionBallPipelineRed pipeline) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    public static boolean ballInView() {
-        return foundBall.compareAndSet(true, true);
-    }
-
-    public static double getBallScore() {
-        return bestScore;
-    }
-
+    // Return Values for Finding Balls
     public static double degreesToBall() {
         double degrees;
         if (centerY/centerX > 0) {
@@ -152,5 +132,36 @@ public class VisionBall implements VisionRunner.Listener<VisionBallPipelineRed>
 
     public static double distnaceToBall() {
         return distance(centerX, centerY);
+    }
+
+    @Override
+    public void copyPipelineOutputs(VisionBallPipelineRed pipeline) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    // Return Values for Finding Balls
+    public static int getBallNumber() {
+        return ballAmount;
+    }
+
+    public static double getBallXOffset() {
+        return centerX - (IMG_WIDTH / 2);
+    }
+    public static double getBallYOffset() {
+        return centerY - (IMG_HEIGHT / 2);
+    }
+
+    public static boolean ballInView() {
+        return foundBall.compareAndSet(true, true);
+    }
+
+    public static int getBallIndex() {
+        return closestBall;
+    }
+
+    public static double getBallScore() {
+        return bestScore;
     }
 }
