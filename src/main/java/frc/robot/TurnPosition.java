964fe486450669e00623a2961d030a2b5ef6b1fa
CalebB <cmbuff18@gmail.com>
2022-02-22 17:09:51 -0500

    Added compare for reverse and regular

--- src/main/java/frc/robot/TurnPosition.java
+++ src/main/java/frc/robot/TurnPosition.java
@@ -12,39 +12,53 @@ public class TurnPosition {
         double regular = intergerCurrentPos + newPosition; // Somewhat unsure
         double regularPlusOffset = (regular + 1) - currentPosition;
         double regularMinusOffset = Math.abs(currentPosition - regular);
+        double reverseBestOffset = 1;
+        double regularBestOffset = 1;
         double bestPos; 
+        boolean reverseBoolean;
         
+        // Regular
         if (regularPlusOffset < regularMinusOffset) {
-            regularBestPos = regular +1; 
+            regularBestPos = regular +1;
+            regularBestOffset = regularPlusOffset; 
         } else if (regularPlusOffset > regularMinusOffset) {
             regularBestPos =  regular; 
+            reverseBestOffset = regularMinusOffset;
         } else {
             regularBestPos = regular + 1; 
+            regularBestOffset = regularPlusOffset; 
         }
 
+        // Reverse 
         double reverse = regularBestPos - Math.abs((intergerCurrentPos+0.5)-regularBestPos); // Very Unsure
         double reversePlusOffset = (reverse + 1) - currentPosition;
         double reverseMinusOffset = Math.abs(currentPosition - reverse); 
 
         if (reversePlusOffset < reverseMinusOffset) {
-            reverseBestPos = reverse + 1; 
+            reverseBestPos = reverse + 1;
+            reverseBestOffset = reversePlusOffset; 
+            
         } else if (reversePlusOffset > reverseMinusOffset) {
             reverseBestPos = reverse; 
+            reverseBestOffset = reverseMinusOffset;
         } else {
             reverseBestPos = reverse + 1; 
+            reverseBestOffset = reversePlusOffset; 
         }
 
-        double reverseOffset = 1; // Calculate
-        double regularOffset = 1; // Calculate 
-        if(reverseOffset < regularOffset) {
-            bestPos = reverseOffset;
-        } else if(reverseOffset > regularOffset) {
-            bestPos = regularOffset;
+        // Regular vs. Reverse
+        if (regularBestOffset < reverseBestOffset) {
+            bestPos = regularBestPos;
+            reverseBoolean = false;
+        } else if (regularBestOffset > reverseBestOffset) {
+            bestPos = reverseBestPos;
+            reverseBoolean = true;
         } else {
-            bestPos = regularOffset;
+            bestPos = regularBestPos;
+            reverseBoolean = false;
         }
 
-        SmartDashboard.putNumber("Optomized Encoder Position", regularBestPos);
-        return regularBestPos; 
+        SmartDashboard.putNumber("Optomized Encoder Position", bestPos);
+        return bestPos; 
     }
 }
