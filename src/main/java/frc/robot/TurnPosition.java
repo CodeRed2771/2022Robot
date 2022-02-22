ee63d6f9ea38d504b42d0d1feafc0f3ca0a6e8f0
CalebB <cmbuff18@gmail.com>
2022-02-21 21:01:17 -0500

    Worked more, need help with calculations for reverse

--- src/main/java/frc/robot/TurnPosition.java
+++ src/main/java/frc/robot/TurnPosition.java
@@ -7,16 +7,44 @@ public class TurnPosition {
         double absoluteCurrentPos = Math.abs(currentPosition);
         double intergerCurrentPos = Math.round(absoluteCurrentPos);
         double currentDecimal = absoluteCurrentPos - intergerCurrentPos;
+        double regularBestPos;
+        double reverseBestPos;
+        double regular = intergerCurrentPos + newPosition; // Somewhat unsure
+        double regularPlusOffset = (regular + 1) - currentPosition;
+        double regularMinusOffset = Math.abs(currentPosition - regular);
         double bestPos; 
-
-        if (currentDecimal > 0.5) {
-            bestPos = intergerCurrentPos + 1 + newPosition; 
-        } else if (currentDecimal < 0.5) {
-            bestPos = intergerCurrentPos + newPosition; 
+        
+        if (regularPlusOffset < regularMinusOffset) {
+            regularBestPos = regular +1; 
+        } else if (regularPlusOffset > regularMinusOffset) {
+            regularBestPos =  regular; 
         } else {
-            bestPos = intergerCurrentPos + 1 + newPosition; 
+            regularBestPos = regular + 1; 
         }
-        SmartDashboard.putNumber("Optomized Encoder Position", bestPos);
-        return bestPos; 
+
+        double reverse = regularBestPos - Math.abs((intergerCurrentPos+0.5)-regularBestPos); // Very Unsure
+        double reversePlusOffset = (reverse + 1) - currentPosition;
+        double reverseMinusOffset = Math.abs(currentPosition - reverse); 
+
+        if (reversePlusOffset < reverseMinusOffset) {
+            reverseBestPos = reverse + 1; 
+        } else if (reversePlusOffset > reverseMinusOffset) {
+            reverseBestPos = reverse; 
+        } else {
+            reverseBestPos = reverse + 1; 
+        }
+
+        double reverseOffset = 1; // Calculate
+        double regularOffset = 1; // Calculate 
+        if(reverseOffset < regularOffset) {
+            bestPos = reverseOffset;
+        } else if(reverseOffset > regularOffset) {
+            bestPos = regularOffset;
+        } else {
+            bestPos = regularOffset;
+        }
+
+        SmartDashboard.putNumber("Optomized Encoder Position", regularBestPos);
+        return regularBestPos; 
     }
 }
