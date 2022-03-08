04d9eb5015051a504be486c265bcec8b282f8d89
Dale Van Voorst <dale@dalevanvoorst.com>
2022-03-08 18:26:20 -0500

    shooter position changes

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -12,35 +12,34 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.ctre.phoenix.sensors.PigeonIMU.CalibrationMode;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 
 public class Shooter {
 
-    private static CANSparkMax shooterMotor = new CANSparkMax(Wiring.SHOOTER_MOTOR_ID, MotorType.kBrushless);
-    private static CANSparkMax feederMotor = new CANSparkMax(Wiring.FEEDER_MOTOR_ID, MotorType.kBrushless);
+    private static TalonFX shooterMotor = new TalonFX(Wiring.SHOOTER_MOTOR_ID);
+    private static TalonFX feederMotor = new TalonFX(Wiring.FEEDER_MOTOR_ID);
 
     private static boolean isInitialized = false;
     private static boolean isEnabled = false;
     private static boolean isGateOpen = false;
     private static boolean oneShot = false;
     private static boolean continuousShooting = false;
+    private static double shooterVelocityTarget = 0;
     private static int timer = 0;
+    private static final int kPIDLoopIdx = 0;
     private static double targetSpeed = Calibration.SHOOTER_DEFAULT_SPEED;
     private static double adjustmentFactor = 1;
-    private static SparkMaxPIDController shooterPID;
-    private static SparkMaxPIDController feederPID;
     private static DoubleSolenoid ballLiftSolenoid;
     private static DoubleSolenoid shooterPositionSolenoid_Stage1;
     private static DoubleSolenoid shooterPositionSolenoid_Stage2;
     public static enum ShooterPosition {
         Low,
         Medium,
-        High
+        Backwards
     }
     private static ShooterPosition curShooterPosition;
     public static enum ManualShotPreset {
@@ -66,86 +65,121 @@ public class Shooter {
         new shooterArrayValue(ShooterPosition.Low, 9000),
         new shooterArrayValue(ShooterPosition.Low, 10000), 
         new shooterArrayValue(ShooterPosition.Low, 12000),
-        new shooterArrayValue(ShooterPosition.Medium, 13000),
-        new shooterArrayValue(ShooterPosition.Medium, 1400),
-        new shooterArrayValue(ShooterPosition.Medium, 1500),
-        new shooterArrayValue(ShooterPosition.Medium, 1600),
-        new shooterArrayValue(ShooterPosition.Medium, 1800),
-        new shooterArrayValue(ShooterPosition.Medium, 1900),
+        new shooterArrayValue(ShooterPosition.Low, 13000),
+        new shooterArrayValue(ShooterPosition.Medium, 2000),
+        new shooterArrayValue(ShooterPosition.Medium, 2500),
+        new shooterArrayValue(ShooterPosition.Medium, 3600),
+        new shooterArrayValue(ShooterPosition.Medium, 4800),
+        new shooterArrayValue(ShooterPosition.Medium, 5900),
     };
 
 
     public static void init() {
-        shooterMotor.restoreFactoryDefaults();
-        shooterMotor.setOpenLoopRampRate(.1);
-        shooterMotor.setSmartCurrentLimit(40);
-        shooterMotor.setIdleMode(IdleMode.kCoast);
+        shooterMotor.configFactoryDefault(10);
         shooterMotor.setInverted(true);
-        
-        shooterPID = shooterMotor.getPIDController();
-        shooterPID.setP(Calibration.SHOOTER_P);
-        shooterPID.setI(0);
-        shooterPID.setD(Calibration.SHOOTER_D);
-        shooterPID.setFF(Calibration.SHOOTER_F);
-        shooterPID.setOutputRange(0, 1);
+        shooterMotor.setSensorPhase(false);
+       
+		/* set the relevant frame periods to be at least as fast as periodic rate */
+		// shooterMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 0);
+        // shooterMotor.setStatusFramePeriod(StatusFrameEnhanced., 10, 0);
 
-        shooterPID.setReference(0, ControlType.kVelocity);  // set to 0 speed
+		/* set the peak and nominal outputs */
+		shooterMotor.configNominalOutputForward(0, 0);
+		shooterMotor.configNominalOutputReverse(0, 0);
+		shooterMotor.configPeakOutputForward(1, 0);
+		shooterMotor.configPeakOutputReverse(-1, 0);
+		shooterMotor.setNeutralMode(NeutralMode.Coast);
+        shooterMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, kPIDLoopIdx, 0);
 
-        feederMotor.restoreFactoryDefaults();
-        feederMotor.setOpenLoopRampRate(.1);
-        feederMotor.setSmartCurrentLimit(40);
-        feederMotor.setIdleMode(IdleMode.kCoast);
-        feederMotor.setInverted(true);
+		shooterMotor.configClosedloopRamp(.45, 0);
+		/* set closed loop gains in slot0 - see documentation */
+        shooterMotor.selectProfileSlot(0, 0);
 
-        feederPID = feederMotor.getPIDController();
-        feederPID.setP(Calibration.SHOOTER_P);
-        feederPID.setI(0);
-        feederPID.setD(Calibration.SHOOTER_D);
-        feederPID.setFF(Calibration.SHOOTER_F);
-        feederPID.setOutputRange(0, 1);
 
-        feederPID.setReference(0, ControlType.kVelocity);  // set to 0 speed
-		 	
+		shooterMotor.config_kP(kPIDLoopIdx, Calibration.SHOOTER_P, 0);
+		shooterMotor.config_kI(kPIDLoopIdx, Calibration.SHOOTER_I, 0);
+		shooterMotor.config_kD(kPIDLoopIdx, Calibration.SHOOTER_D, 0);
+        shooterMotor.config_kF(kPIDLoopIdx, Calibration.SHOOTER_F, 0);
+		/* zero the sensor */
+        shooterMotor.setSelectedSensorPosition(0, kPIDLoopIdx, 0);
+ 	
+
+        feederMotor.configFactoryDefault(10);
+        feederMotor.setInverted(false);
+        feederMotor.setSensorPhase(true);
+       
+		/* set the relevant frame periods to be at least as fast as periodic rate */
+		// shooterMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 0);
+        // shooterMotor.setStatusFramePeriod(StatusFrameEnhanced., 10, 0);
+
+		/* set the peak and nominal outputs */
+		feederMotor.configNominalOutputForward(0, 0);
+		feederMotor.configNominalOutputReverse(0, 0);
+		feederMotor.configPeakOutputForward(1, 0);
+		feederMotor.configPeakOutputReverse(-1, 0);
+		feederMotor.setNeutralMode(NeutralMode.Coast);
+        feederMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, kPIDLoopIdx, 0);
+
+		feederMotor.configClosedloopRamp(.45, 0);
+		/* set closed loop gains in slot0 - see documentation */
+        feederMotor.selectProfileSlot(0, 0);
+
+
+		feederMotor.config_kP(kPIDLoopIdx, Calibration.SHOOTER_P, 0);
+		feederMotor.config_kI(kPIDLoopIdx, Calibration.SHOOTER_I, 0);
+		feederMotor.config_kD(kPIDLoopIdx, Calibration.SHOOTER_D, 0);
+        feederMotor.config_kF(kPIDLoopIdx, Calibration.SHOOTER_F, 0);
+		/* zero the sensor */
+        feederMotor.setSelectedSensorPosition(0, kPIDLoopIdx, 0);
+        	
 		SmartDashboard.putNumber("Shoot P", Calibration.SHOOTER_P);
 		SmartDashboard.putNumber("Shoot D", Calibration.SHOOTER_D);
         SmartDashboard.putNumber("Shoot F", Calibration.SHOOTER_F);
         SmartDashboard.putNumber("Shoot Setpoint", Calibration.SHOOTER_DEFAULT_SPEED);
         SmartDashboard.putNumber("Feeder Setpoint", Calibration.FEEDER_DEFAULT_SPEED);
-        SmartDashboard.putBoolean("Shooter TUNE", false);
+        SmartDashboard.putBoolean("Shooter TUNE", true);
 
         isInitialized = true;
 
         ballLiftSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, Wiring.BALLLIFT_FOWARD, Wiring.BALLLIFT_REVERSE);
         shooterPositionSolenoid_Stage1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, Wiring.SHOOTERPOSITION_STAGE1_FOWARD, Wiring.SHOOTERPOSITION_STAGE1_REVERSE);
-        shooterPositionSolenoid_Stage1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, Wiring.SHOOTERPOSITION_STAGE2_FOWARD, Wiring.SHOOTERPOSITION_STAGE2_REVERSE);
+        shooterPositionSolenoid_Stage2 = new DoubleSolenoid(PneumaticsModuleType.REVPH, Wiring.SHOOTERPOSITION_STAGE2_FOWARD, Wiring.SHOOTERPOSITION_STAGE2_REVERSE);
+        
+        setBallLiftDown();
+
     }
 
     public static void tick() {
-        double shooterVelocityTarget = 0;
 
         if (!isInitialized) 
             return;
 
             if (isEnabled) {
-                shooterPID.setReference(shooterVelocityTarget, ControlType.kVelocity);
-                feederPID.setReference(SmartDashboard.getNumber("Feeder Setpoint", Calibration.FEEDER_DEFAULT_SPEED), ControlType.kVelocity);
-                calibrationMode = SmartDashboard.getBoolean("Shooter TUNE", true);
                 if (calibrationMode) {
-                    shooterPID.setFF(SmartDashboard.getNumber("Shoot F", 0));
-                    shooterPID.setP(SmartDashboard.getNumber("Shoot P", 0));
-                    shooterPID.setD(SmartDashboard.getNumber("Shoot D", 0));
-                   
-                    feederPID.setFF(SmartDashboard.getNumber("Shoot F", 0));
-                    feederPID.setP(SmartDashboard.getNumber("Shoot P", 0));
-                    feederPID.setD(SmartDashboard.getNumber("Shoot D", 0));
+                  	shooterMotor.config_kF(kPIDLoopIdx, SmartDashboard.getNumber("Shoot F", 0), 0);
+                    shooterMotor.config_kP(kPIDLoopIdx, SmartDashboard.getNumber("Shoot P", 0), 0);
+                    shooterMotor.config_kI(kPIDLoopIdx, SmartDashboard.getNumber("Shoot I", 0), 0);
+                    shooterMotor.config_kD(kPIDLoopIdx, SmartDashboard.getNumber("Shoot D", 0), 0);
+
+                    feederMotor.config_kF(kPIDLoopIdx, SmartDashboard.getNumber("Shoot F", 0), 0);
+                    feederMotor.config_kP(kPIDLoopIdx, SmartDashboard.getNumber("Shoot P", 0), 0);
+                    feederMotor.config_kI(kPIDLoopIdx, SmartDashboard.getNumber("Shoot I", 0), 0);
+                    feederMotor.config_kD(kPIDLoopIdx, SmartDashboard.getNumber("Shoot D", 0), 0);
+
                     int dist = (int)Math.round(Vision.getDistanceFromTarget());
                     SmartDashboard.putNumber("Distance to Target", dist);
-                    shooterVelocityTarget = SmartDashboard.getNumber("Shoot Setpoint", Calibration.SHOOTER_DEFAULT_SPEED);
-
+                    // shooterVelocityTarget = SmartDashboard.getNumber("Shoot Setpoint", Calibration.SHOOTER_DEFAULT_SPEED);
+                    
                }
+               
+               shooterMotor.set(ControlMode.Velocity, shooterVelocityTarget);
+               feederMotor.set(ControlMode.Velocity, shooterVelocityTarget);
 
-                SmartDashboard.putNumber("SHOOTER VELOCITY", shooterMotor.getEncoder().getVelocity());
-                SmartDashboard.putNumber("SHOOTER ERROR", shooterVelocityTarget - shooterMotor.getEncoder().getVelocity());
+               calibrationMode = SmartDashboard.getBoolean("Shooter TUNE", false);
+              
+
+                SmartDashboard.putNumber("SHOOTER VELOCITY", shooterMotor.getSelectedSensorVelocity());
+                SmartDashboard.putNumber("SHOOTER ERROR", shooterVelocityTarget - shooterMotor.getSelectedSensorVelocity());
                 
                 System.out.println(timer);
 
@@ -206,8 +240,8 @@ public class Shooter {
         oneShot = false;
         continuousShooting = false;
         resetTimer();
-        shooterPID.setReference(0, ControlType.kVelocity);  // set to 0 speed
-        feederPID.setReference(0, ControlType.kVelocity);
+        shooterMotor.set(ControlMode.Velocity,0);
+        feederMotor.set(ControlMode.Velocity, 0);
     }
 
     public static void setAdjustmentFactor (double adjustmentFactor) {
@@ -220,7 +254,7 @@ public class Shooter {
         if (!isInitialized) 
             return 0;
         else
-            return shooterMotor.getEncoder().getVelocity();
+            return shooterMotor.getSelectedSensorVelocity();
     }
 
     public static boolean isAtSpeed() {
@@ -241,7 +275,7 @@ public class Shooter {
         oneShot = true;
         continuousShooting = false;
         if (calibrationMode) {
-            manualVisionOverride = false;
+            manualVisionOverride = true;
         }
         if (!manualVisionOverride) {
             AutoBaseClass mAutoProgram;
@@ -271,14 +305,14 @@ public class Shooter {
     public static void setShooterPosition(ShooterPosition position) {
         switch (position) {
             case Low:
-                shooterPositionSolenoid_Stage1.set(DoubleSolenoid.Value.kForward);
-                shooterPositionSolenoid_Stage2.set(DoubleSolenoid.Value.kReverse);
-                break;
-            case Medium:
                 shooterPositionSolenoid_Stage1.set(DoubleSolenoid.Value.kReverse);
                 shooterPositionSolenoid_Stage2.set(DoubleSolenoid.Value.kReverse);
                 break;
-            case  High:
+            case Medium:
+                shooterPositionSolenoid_Stage1.set(DoubleSolenoid.Value.kForward);
+                shooterPositionSolenoid_Stage2.set(DoubleSolenoid.Value.kReverse);
+                break;
+            case  Backwards:
                 shooterPositionSolenoid_Stage1.set(DoubleSolenoid.Value.kForward);
                 shooterPositionSolenoid_Stage2.set(DoubleSolenoid.Value.kForward);
                 break;
@@ -307,7 +341,7 @@ public class Shooter {
             case Medium:
                 power = 30;
                 break;
-            case High:
+            case Backwards:
                 power = 40;
                 break;
         }
@@ -315,32 +349,28 @@ public class Shooter {
     }
 
     public static void setManualPresets(ManualShotPreset position) {
-        setShooterPosition(ShooterPosition.Low);
+
+        setShooterPosition(ShooterPosition.Medium);
+        
         switch(position) {
             case SuperCloseLowShot:
-                setShooterVelocity(5000);
+                shooterVelocityTarget = 5000;
                 break;
             case BackOfTarmac:
-                setShooterVelocity(8000);
+                shooterVelocityTarget = 6000;
                 break;
             case TarmacLine:
-                setShooterVelocity(1200);
+                shooterVelocityTarget = 6000;
                 break;
         }
+        StartShooter();
         manualVisionOverride = true;
-    }
-
-
-    public static void setShooterVelocity(double rpm) {
-        shooterMotor.set(rpm);
-        feederMotor.set(rpm);
+        
     }
 
     public static void setupShooterAuto() {
         int dis = (int)Math.round(Vision.getDistanceFromTarget());
-        ShooterPosition pos = shooterArray[dis].position;
-        double pow = shooterArray[dis].speed;
-        setShooterPosition(pos);
-        setShooterVelocity(pow);
+        setShooterPosition(shooterArray[dis].position);
+        shooterVelocityTarget = shooterArray[dis].speed;
     }
 }
