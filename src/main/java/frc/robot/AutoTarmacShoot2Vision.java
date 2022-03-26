2a0d5befe4bc025ee01a93690dafe308976556a0
Elisha B <ejbuff16@gmail.com>
2022-03-26 17:57:58 -0400

    added step numbers

--- src/main/java/frc/robot/AutoTarmacShoot2Vision.java
+++ src/main/java/frc/robot/AutoTarmacShoot2Vision.java
@@ -19,31 +19,31 @@ public class AutoTarmacShoot2Vision extends AutoBaseClass {
             SmartDashboard.putNumber("Auto Step", getCurrentStep());
             double degrees = 0;
             switch (getCurrentStep()) {
-                case 0:
+                case 0://step 1
                     VisionShooter.setLED(true);
                     driveInches(64, 0, .8);
                     setTimerAndAdvanceStep(6000);
                     break;
-                case 1:
+                case 1://step 2
                     if (driveCompleted()) {
                         advanceStep();
                     }
                     break;
-                case 2:
+                case 2://step 3
                     Shooter.alignAndShoot();
                     setTimerAndAdvanceStep(1000);
-                case 3:
+                case 3://step 4
                     break;
-                case 4:
+                case 4://step 5
                     Intake.startIntake();
                     advanceStep();
                     break;
-                case 5:
+                case 5://step 6
                     setTimerAndAdvanceStep(1000);
                     break;
-                case 6:
+                case 6://step 7
                     break;
-                case 7:
+                case 7://step 8
                     if (VisionBall.ballInView()) {
                         setStep(20);
                     } else {
@@ -51,17 +51,17 @@ public class AutoTarmacShoot2Vision extends AutoBaseClass {
                         setTimerAndAdvanceStep(1500);
                     }
                     break;
-                case 8:
+                case 8://step 9
                     if (turnCompleted()) {
                         advanceStep();
                     }
                     break;
-                case 9:
+                case 9://step 10
                     setTimerAndAdvanceStep(1000);
                     break;
-                case 10:
+                case 10://step 11
                     break;
-                case 11:
+                case 11://step 12
                     if (VisionBall.ballInView()) {
                         setStep(20);
                     } else {
@@ -69,47 +69,47 @@ public class AutoTarmacShoot2Vision extends AutoBaseClass {
                         setTimerAndAdvanceStep(2500);
                     }
                     break;
-                case 12:
+                case 12://step 13
                     if (turnCompleted()) {
                         advanceStep();
                     }
                     break;
-                case 13:
+                case 13://step 14
                     setTimerAndAdvanceStep(1000);
                     break;
-                case 14:
+                case 14://step 15
                     break;
-                case 15:
+                case 15://step 16
                     if (VisionBall.ballInView()) {
                         setStep(20);
                     } else {
                         setStep(24);
                     }
                     break;
-                case 20:
+                case 20://step 17
                     double ballDistance = VisionBall.distanceToBall();
                     if(ballDistance > 36)
                         ballDistance = 36;
                     driveInches(ballDistance, VisionBall.degreesToBall(), .8);
                     setTimerAndAdvanceStep(7000);
                     break;
-                case 21: 
+                case 21://step 18
                     if (driveCompleted()){
                         advanceStep();
                     }
                     break;
-                case 22:
+                case 22://step 19
                     driveInches(12,0,.8);
                     setTimerAndAdvanceStep(7500);
                     break;
-                case 23:
+                case 23://step 20
                     if(driveCompleted()) {
                         advanceStep();
                     }
-                case 24:
+                case 24://step 21
                     Shooter.alignAndShoot();
                     setTimerAndAdvanceStep(1000);
-                case 25:
+                case 25://step 22
                     break;
                 case 26://end
                     stop();
